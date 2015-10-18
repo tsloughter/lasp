3a8ce34dd63f8e87024f53a889cbbd7b249b56ba
Tristan Sloughter <t@crashfast.com>
2015-10-18 09:24:38 -0500

    simplify and use rebar3

--- samples/docker.sh
+++
@@ -1,5 +0,0 @@
-# Pull the docker image that contains Lasp
-$ docker pull cmeiklejohn/lasp
-
-# Start up the docker image with host networking and pseudo-tty
-$ docker run -i -t --net=host cmeiklejohn/lasp
